0c5ba87b4d4e75ce5a7179d8704607273ea19a9e
Barr, Clinton (ESD) <CBarr@esd.wa.gov>
2017-08-17 14:39:16 -0700

    Fixed anchor links to groups with periods in the name

--- format-groups-to-md.ps1
+++ format-groups-to-md.ps1
@@ -58,21 +58,21 @@ try {
     MdAdd("### Table of Contents`n")
     MdAdd("Azure Resource Groups:")
     foreach($group in $groups) {
-        $anchorLink = MdAnchor -DisplayText "$($group.name)" -HeadingText "Group $($group.name)"
+        $anchorLink = MdAnchor -DisplayText "$($group.name)" -HeadingText "Group $($group.name -replace '\.',' ')"
         MdAdd(" * $anchorLink")
     }
     MdAdd("")
 
     # Added Group Tables
     foreach($group in $groups) {
-        MdAdd("## Group $($group.name)`n")
+        MdAdd("## Group $($group.name -replace '\.',' ')`n")
         MdAdd("Azure Portal Link: [$($group.name)]($($azResourceLink + $group.id))`n")
         $groupTable = [MdTable]::New(@("Resource","Link","Type","Location"))
         foreach($resource in $group.resources) {
             $url = UrlEncode($azResourceLink + $resource.id)
             if ($null -ne $resource.properties.name) { $name = $resource.properties.name }
             elseif ($null -ne $resource.name) { $name = $resource.name }
-            $groupTable.AddRow(@("$name","[Azure Portal]($url)","$($resource.type)","$($resource.location)"))
+            $groupTable.AddRow(@("$name","[Azure Portal]($url)","``$($resource.type)``","$($resource.location)"))
         }
         MdAdd($groupTable.table + "`n")
     }
